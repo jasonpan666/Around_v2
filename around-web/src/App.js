fef2b3c295ce9f21494d9ec6067c9cb796360c6b
jasonpan666 <panzhenyujason@gmail.com>
2019-02-13 02:40:38 -0800

    Main Register

--- around-web/src/App.js
+++ around-web/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { TopBar } from './TopBar';
+import { Main } from './Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopBar/>
+        <Main/>
       </div>
     );
   }
---
+++ around-web/src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Register } from './Register';
+
+export class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <Register/>
+      </div>
+    );
+  }
+}
