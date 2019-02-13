d958027dfd80b94be23a20d213d58a590a5cd4ce
jasonpan666 <panzhenyujason@gmail.com>
2019-02-13 02:24:17 -0800

    TopBar

--- around-web/src/App.js
+++ around-web/src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TopBar } from './TopBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className="App-title">Around</div>
-        </header>
+        <TopBar/>
       </div>
     );
   }
---
+++ around-web/src/TopBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+export class TopBar extends React.Component {
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-title">Around</div>
+      </header>
+    );
+  }
+}
