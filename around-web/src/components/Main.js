5c87b63221b839e28c9e61d0c5ae1e28ccc6fd00
jasonpan666 <panzhenyujason@gmail.com>
2019-02-15 00:17:12 -0800

    react router and switch

--- around-web/src/components/Main.js
+++ around-web/src/components/Main.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Register } from './Register';
 import { Login } from './Login';
+import { Switch, Route } from 'react-router-dom';
 
 export class Main extends React.Component {
   render() {
     return (
       <div className="main">
-        <Login/>
+        <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/register" component={Register}/>
+          <Route component={Login}/>
+        </Switch>
       </div>
     );
   }
