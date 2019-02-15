b9995f139999ee20a93ad4c5ab97d971e577d4a6
jasonpan666 <panzhenyujason@gmail.com>
2019-02-15 00:28:28 -0800

    register and login switch

--- around-web/src/components/Login.js
+++ around-web/src/components/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Form, Icon, Input, Button, message } from 'antd';
 import { API_ROOT } from '../constants';
+import { Link } from 'react-router-dom';
 
 class NormalLoginForm extends React.Component {
   handleSubmit = (e) => {
@@ -53,7 +54,7 @@ class NormalLoginForm extends React.Component {
           <Button type="primary" htmlType="submit" className="login-form-button">
             Log in
           </Button>
-          Or <a href="">register now!</a>
+          Or <Link to="/register">register now!</Link>
         </Form.Item>
       </Form>
     );
--- around-web/src/components/Register.js
+++ around-web/src/components/Register.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Form, Input, Button, message } from 'antd';
 import { API_ROOT } from '../constants';
+import { Link } from 'react-router-dom';
 
 class RegistrationForm extends React.Component {
   state = {
@@ -129,6 +130,7 @@ class RegistrationForm extends React.Component {
         </Form.Item>
         <Form.Item {...tailFormItemLayout}>
           <Button type="primary" htmlType="submit">Register</Button>
+          <p>I already have an account. Go back to <Link to="/login">login</Link>.</p>
         </Form.Item>
       </Form>
     );
