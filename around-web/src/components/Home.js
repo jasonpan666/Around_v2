ff03b38e2290e5e37536d2ed71b837b22f636937
jasonpan666 <panzhenyujason@gmail.com>
2019-02-18 14:13:50 -0800

    tab bar init

--- around-web/src/components/Home.js
+++ around-web/src/components/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Tabs, Button } from 'antd';
+
+const TabPane = Tabs.TabPane;
 
 export class Home extends React.Component {
   render() {
+    const operations = <Button type="primary">Create New Post</Button>;
+
     return (
-      <div>This is Home!</div>
-    )
+      <Tabs className="main-tabs" tabBarExtraContent={operations}>
+        <TabPane tab="Image Posts" key="1">
+          <div>
+            <h1>hello world</h1>
+          </div>
+        </TabPane>
+        <TabPane tab="Video Posts" key="2">Content of tab 2</TabPane>
+        <TabPane tab="Map" key="3">Content of tab 3</TabPane>
+      </Tabs>
+    );
   }
 }
